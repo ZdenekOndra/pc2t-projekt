d124b5593e31453a9257cbdd1459a8d2658c37f3
19506 <luky.makovicka@seznam.cz>
2026-05-05 18:16:29 +0200

    update

--- src/main/java/employeedb/SecuritySpecialist.java
+++ src/main/java/employeedb/SecuritySpecialist.java
@@ -30,4 +30,23 @@ public class SecuritySpecialist extends Employee {
         System.out.println("Prumerna kvalita spoluprace: " + avgQuality);
         System.out.println("Rizikove skore: " + score);
 
+        String level;
+        if (score < 5) {
+            level = "NIZKE";
+        } else if (score < 15) {
+            level = "STREDNI";
+        } else {
+            level = "VYSOKE";
+        }
+        System.out.println("Uroven rizika: " + level);
+    }
+
+    public String getGroupName() {
+        return "Bezpecnostni specialista";
+    }
+
+    public char getGroupCode() {
+        return 'S';
+    }
+}
       
